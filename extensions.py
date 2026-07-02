ec58bcbc49b549ace946a203e71e22301d53b51f
praveenkumar666666 <dharavathpraveen545@gmail.com>
2026-07-02 11:31:43 +0530

    Fix extensions and requirements

--- extensions.py
+++ extensions.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_socketio import SocketIO
 
 # Database
 db = SQLAlchemy()
@@ -16,3 +17,6 @@ bcrypt = Bcrypt()
 
 # Database Migration
 migrate = Migrate()
+
+# SocketIO
+socketio = SocketIO()
